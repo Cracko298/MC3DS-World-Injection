25b0256b6e7dc6b903f0ec77d4dd2536b243ebc5
Phinehas Beresford <78656905+Cracko298@users.noreply.github.com>
2024-02-23 11:25:32 -0500

    Add files via upload

---
+++ src/python/cstm_py.py
@@ -0,0 +1,23 @@
+import os
+from mc3dslib import BlangFile
+from sys import argv
+
+def to_JSON(file,out):
+    blangFile = BlangFile().open(file)
+    os.get_exec_path()
+    blangFile.toJson(out)
+
+def to_BLANG(file,out):
+    blangFile = BlangFile().fromJson(file)
+    blangFile.export(out)
+
+file_path = argv[1]
+mode = argv[2]
+file_path = file_path.replace('"','')
+out_path = f"{os.path.dirname(file_path)}\\out"
+os.makedirs(f"{out_path}",exist_ok=True)
+
+if mode == "1":
+    to_JSON(file_path,out_path)
+else:
+    to_BLANG(file_path,out_path)
